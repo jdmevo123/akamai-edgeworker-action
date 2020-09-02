8168f995c09a1d9f6c5ed3cdabb258812724af96
jdmevo123 <dale_lenard@hotmail.com>
2020-09-02 12:09:20 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,43 +25,27 @@ tar -czvf ~/deploy.tar.gz main.js bundle.json utils
 if [ -n "$edgeworkersID" ]; then
    echo "Uploading Edgeworker Version"
    #UPLOAD edgeWorker
-   akamai edgeworkers upload \
+   ewupload=$(akamai edgeworkers upload \
      --edgerc ~/.edgerc \
      --section edgeworkers \
      --bundle ~/deploy.tar.gz \
-     ${edgeworkersID} |
-         while read line; do
+     ${edgeworkersID})
+   edgeworkersVersion=$(echo $(<$GITHUB_WORKSPACE/bundle.json) | jq '.["edgeworker-version"]' | tr -d '"')
+   echo "Activating Edgeworker Version: ${edgeworkersVersion}"
+   #ACTIVATE  edgeworker
+   echo "activating"
+   akamai edgeworkers activate \
+         --edgerc ~/.edgerc \
+         --section edgeworkers \
+         ${edgeworkersID} \
+         ${network} \
+         ${edgeworkersVersion} |
+           while read line; do
              if [[ $line =~ status ]] ; then
                 status=`echo $line | tr -d -c 0-9`
-                echo "Status is: $status"
                 case $status in
                    200) echo "Activation Successfull" ;;
-                   201) edgeworkersVersion=$(echo $(<$GITHUB_WORKSPACE/bundle.json) | jq '.["edgeworker-version"]' | tr -d '"')
-                        echo "Activating Edgeworker Version: ${edgeworkersVersion}"
-                        #ACTIVATE  edgeworker
-                        echo "activating"
-                        akamai edgeworkers activate \
-                              --edgerc ~/.edgerc \
-                              --section edgeworkers \
-                              ${edgeworkersID} \
-                              ${network} \
-                              ${edgeworkersVersion} |
-                                while read line; do
-                                  if [[ $line =~ status ]] ; then
-                                     status=`echo $line | tr -d -c 0-9`
-                                     case $status in
-                                        200) echo "Activation Successfull" ;;
-                                        201) echo "Activation Successfull" ;;
-                                        400) echo "Previous activation still pending ... aborting" && exit 1 ;;
-                                        401) echo "Invalid authorization credentials ... aborting" && exit 1 ;;
-                                        403) echo "The client is not authorized to invoke the service ... aborting" && exit 1 ;;
-                                        422) echo "System limit reached ... aborting" && exit 1 ;;
-                                        502) echo "Gateway unavailable to process request ... aborting" && exit 1 ;;
-                                        503) echo "Service is temporarily unavailable ... aborting" && exit 1 ;;
-                                        *)   echo "$status!!  Activation: status not defined ... aborting" && exit 1 ;;
-                                     esac
-                                  fi
-                                done ;;
+                   201) echo "Activation Successfull" ;;
                    400) echo "Previous activation still pending ... aborting" && exit 1 ;;
                    401) echo "Invalid authorization credentials ... aborting" && exit 1 ;;
                    403) echo "The client is not authorized to invoke the service ... aborting" && exit 1 ;;
@@ -70,7 +54,7 @@ if [ -n "$edgeworkersID" ]; then
                    503) echo "Service is temporarily unavailable ... aborting" && exit 1 ;;
                    *)   echo "$status!!  Activation: status not defined ... aborting" && exit 1 ;;
                 esac
-             fi
+             fi                    
            done
 fi
 if [ -z "$edgeworkersID" ]; then
